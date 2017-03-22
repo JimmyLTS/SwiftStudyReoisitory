c90f6ee8500347877ce75efc5f6f24a9646c205c
吕同生 <lvtongsheng@iscs.com.cn>
2017-03-22 17:40:20 +0800

    first commit

---
+++ HelloWorld-Objective-C/HelloWorld-Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld-Objective-C
+//
+//  Created by 吕同生 on 17/2/16.
+//  Copyright © 2017年 JimmyLTS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld-Objective-C/HelloWorld-Objective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld-Objective-C
+//
+//  Created by 吕同生 on 17/2/16.
+//  Copyright © 2017年 JimmyLTS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
